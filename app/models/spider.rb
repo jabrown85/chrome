b51f2826aafe66e78bd336d349ec7736f982b9fb
drewtunney <bryantunney@gmail.com>
2018-09-06 11:20:42 -0400

    add options

--- app/models/spider.rb
+++ app/models/spider.rb
@@ -6,9 +6,9 @@ class Spider
     # make a directory for chrome if it doesn't already exist
     chrome_dir = File.join Dir.pwd, %w(tmp chrome)
     FileUtils.mkdir_p chrome_dir
-    user_data_dir = "--user-data-dir=#{chrome_dir}"
-    # add the option for user-data-dir
-    options.add_argument user_data_dir
+    # user_data_dir = "--user-data-dir=#{chrome_dir}"
+    # # add the option for user-data-dir
+    # options.add_argument user_data_dir
 
     # let Selenium know where to look for chrome if we have a hint from
     # heroku. chromedriver-helper & chrome seem to work out of the box on osx,
@@ -22,9 +22,12 @@ class Spider
     end
 
     # headless!
-    options.add_argument "window-size=1200x600"
-    options.add_argument "headless"
-    options.add_argument "disable-gpu"
+    # options.add_argument "window-size=1200x600"
+    # options.add_argument "headless"
+    # options.add_argument "disable-gpu"
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
 
     # make the browser
     Watir::Browser.new :chrome, options: options
